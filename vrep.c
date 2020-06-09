5f2654f361508af4a9e97986d631411d170471f4
ParvatiNair <64624653+ParvatiNair@users.noreply.github.com>
2020-06-09 16:57:31 +0530

    updated

--- vrep.c
+++ vrep.c
@@ -14,11 +14,14 @@ int main(int argc,char* argv[])
     int leftMotorBackHandle;
     int rightMotorBackHandle; 
     int sensorHandle;
-    
+    float dist;
     // number of variables passed from lua to this code + 1
     // (+1 because the first value in argv is always the location of this program)
     int n = 8;
-
+    //int i=0;
+    int a=1;
+    int c=2;
+    int e=6;
     // save the handles
     if (argc>=n){
         portNb=atoi(argv[1]);
@@ -36,7 +39,7 @@ int main(int argc,char* argv[])
     }
 
     // variables for the code
-    
+    float position[3] = {0,0,0};
     float leftMotorFrontSpeed = 1;
     float rightMotorFrontSpeed = 1;
     float leftMotorBackSpeed = 1;
@@ -44,7 +47,11 @@ int main(int argc,char* argv[])
     float* detectedPoint = NULL;
     int* detectedObjectHandle = NULL;
     float* detectedSurfaceNormalVector = NULL;
-
+    unsigned char *detectionstate=0;
+    int* b= &a;
+    int* d= &c;
+    int* f= &e;
+    int* z= &sensorHandle;
     // functions or variable names starting with 'simx' are coppeliamsim functions
     // for details on these functions refer: https://www.coppeliarobotics.com/helpFiles/en/remoteApiFunctions.htm
 
@@ -53,30 +60,48 @@ int main(int argc,char* argv[])
 
     if (clientID!=-1){
         printf("connection established\n");
-    
-        /******************** SETUP ******************************/
+
+        /******* SETUP ***********/
         // write code here that you wish to perform ONCE
-        
+        simxGetObjectPosition(clientID, botHandle, -1, position, simx_opmode_streaming);
         simxSetJointTargetVelocity(clientID,leftMotorFrontHandle,leftMotorFrontSpeed,simx_opmode_oneshot);
         simxSetJointTargetVelocity(clientID,rightMotorFrontHandle,rightMotorFrontSpeed,simx_opmode_oneshot);
 	    simxSetJointTargetVelocity(clientID,leftMotorBackHandle,leftMotorBackSpeed,simx_opmode_oneshot);
         simxSetJointTargetVelocity(clientID,rightMotorBackHandle,rightMotorBackSpeed,simx_opmode_oneshot);
         simxReadProximitySensor(clientID, sensorHandle, NULL, detectedPoint, detectedObjectHandle,detectedSurfaceNormalVector,simx_opmode_streaming);
-        /*********************************************************/
-        
-        while (simxGetConnectionId(clientID)!=-1){  
-            /************************ LOOP *****************************/
+        simxGetObjectGroupData(clientID, sim_object_proximitysensor_type, 13, b, &z, d, &detectedObjectHandle, f, &detectedPoint, NULL,NULL,simx_opmode_streaming);
+        /*******************/
+
+        while (simxGetConnectionId(clientID)!=-1){
+            //i++;  
+            /******** LOOP ***********/
+
             // write code here that you wish to REPEAT
-           
-            if( simxReadProximitySensor(clientID, sensorHandle, NULL, detectedPoint, detectedObjectHandle, detectedSurfaceNormalVector, simx_opmode_buffer) == simx_return_ok){
-                printf("sensor: %f %f %f  \n", &detectedPoint);
+		simxGetObjectPosition(clientID, botHandle, -1, position, simx_opmode_streaming);
+        simxSetJointTargetVelocity(clientID,leftMotorFrontHandle,leftMotorFrontSpeed,simx_opmode_oneshot);
+        simxSetJointTargetVelocity(clientID,rightMotorFrontHandle,rightMotorFrontSpeed,simx_opmode_oneshot);
+	    simxSetJointTargetVelocity(clientID,leftMotorBackHandle,leftMotorBackSpeed,simx_opmode_oneshot);
+        simxSetJointTargetVelocity(clientID,rightMotorBackHandle,rightMotorBackSpeed,simx_opmode_oneshot);
+        simxReadProximitySensor(clientID, sensorHandle, NULL, detectedPoint, detectedObjectHandle,detectedSurfaceNormalVector,simx_opmode_streaming);
+        simxGetObjectGroupData(clientID, sim_object_proximitysensor_type, 13, b, &z, d, &detectedObjectHandle, f, &detectedPoint, NULL,NULL,simx_opmode_streaming);
+
+            if( simxGetObjectPosition(clientID, botHandle, -1, position, simx_opmode_buffer) == simx_return_ok)
+                printf("the pos: %f %f %f\n",position[0],position[1],position[2]);
+            if( simxReadProximitySensor(clientID, sensorHandle, detectionstate, detectedPoint, detectedObjectHandle, detectedSurfaceNormalVector, simx_opmode_buffer) == simx_return_ok){
+                //printf("sensor: %f %f %f  \n", detectedPoint[0],detectedPoint[1],detectedPoint[2]);
+                dist= sqrt(detectedPoint[0]*detectedPoint[0]+detectedPoint[1]*detectedPoint[1]+detectedPoint[2]*detectedPoint[2]);
+                printf("Distance:%f\n", dist);
+                //printf("state: %u\n",detectionstate);
+                //printf("Object handle:%f\n", detectedObjectHandle);
             }
-            /***********************************************************/
+            /*********************/
+            //printf("%d\n",i);
         }
-        
+
         // end communication with lua
         simxFinish(clientID);
 
     }
     printf("Simulation Stopped!");
+    
 }
